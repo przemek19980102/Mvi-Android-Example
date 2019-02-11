ff0f29e8194d59dca12582e89b4185a7f9f08560
JakubKomorowski <jakub.komorowski@codequest.com>
2019-02-11 09:47:26 +0100

    Fixed RandomActivityTest

--- app/src/androidTest/java/com/example/appName/presentation/random/RandomActivityTest.kt
+++ app/src/androidTest/java/com/example/appName/presentation/random/RandomActivityTest.kt
@@ -6,34 +6,42 @@ import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.rule.ActivityTestRule
 import com.example.appName.R
+import com.example.appName.presentation.random.di.RANDOM_SCOPE
 import io.reactivex.Observable
 import io.reactivex.subjects.BehaviorSubject
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.koin.dsl.module.module
+import org.koin.standalone.StandAloneContext.loadKoinModules
+import org.koin.test.KoinTest
 
-
-class RandomActivityTest {
+class RandomActivityTest : KoinTest {
     private val viewStateSubject = BehaviorSubject.create<RandomViewState>()
 
     @get:Rule
-    val activityTestRule: ActivityTestRule<RandomActivity> = ActivityTestRule<RandomActivity>(RandomActivity::class.java, true, true)
-
-    @Before
-    fun intentWithStubbedNoteId() {
-        activityTestRule.activity.apply {
-            presenter = object : RandomPresenter {
-                override fun getCurrentViewState(): RandomViewState? = viewStateSubject.value
-
-                override fun dispose() {}
-
-                override fun getStateObservable(): Observable<RandomViewState> = viewStateSubject
-
+    val activityTestRule: ActivityTestRule<RandomActivity> =
+            object : ActivityTestRule<RandomActivity>(RandomActivity::class.java, true, true) {
+                override fun beforeActivityLaunched() {
+                    overrideKoinModules()
+                }
             }
 
-            subscribeToViewState()
+    private fun overrideKoinModules() {
+        val element = module(override = true) {
+            scope<RandomPresenter>(RANDOM_SCOPE) {
+                object : RandomPresenter {
+                    override fun getCurrentViewState(): RandomViewState? = viewStateSubject.value
+
+                    override fun dispose() {}
+
+                    override fun getStateObservable(): Observable<RandomViewState> = viewStateSubject
+
+                }
+            }
         }
+        loadKoinModules(listOf(element))
     }
+
 
     @Test
     fun whenViewStateWithFirstNumberIsEmittedFirstNumberViewIsSetToItsValue() {
